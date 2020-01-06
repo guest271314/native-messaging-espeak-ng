f6e74bafe9ddcfa49434731044462815ac02b28d
guest271314 <guest271314@gmail.com>
2020-01-06 03:07:59 +0000

    Create external-connection.js

---
+++ app/external-connection.js
@@ -0,0 +1,22 @@
+// native-messaging-espeak-ng guest271314 1-4-2020
+// espeak-ng using Native Messaging, Native File System, JavaScript
+const externalConnection = _ => {
+  chrome.runtime.onConnectExternal.addListener(externalPort => {
+    console.log({
+      externalPort
+    });
+    externalPort.onMessage.addListener(async message => {
+      if (nativeMessagingEspeakNG) {
+        // result: ArrayBuffer, not transferrable
+        const {
+          input, phonemes, result
+        // message: Text, or SSML markup
+        } = await nativeMessagingEspeakNG(message);
+        externalPort.postMessage({
+          input, phonemes, result: [...new Uint8Array(result)]
+        });
+      };
+    });
+  });
+}
+window.onload = externalConnection;
